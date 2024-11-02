fd222bdb661fee875ecf6eedd0dd3d8b072d0a49
Valéria Aires <141928476+AffVall@users.noreply.github.com>
2024-11-02 15:59:26 -0300

    Add files via upload

---
+++ Exercicios/Main.py
@@ -0,0 +1,106 @@
+import Uteis
+from Menu import *
+
+EX = ('FIM', 'PG', 'DIVISAO', 'URL', 'CADASTRO', 'CPF')
+titulo('Benvindo ao Arquivo de exercicios :D')
+while True:
+    escolha = op_menu(EX, 'Aula')
+    # Saindo do programa
+    if escolha == EX[0] or escolha == 0 :
+        titulo('Codigo encerrado.')
+        break
+
+#Curso {Curso em Video (Mundo 3)}
+    #PG
+    elif escolha == EX[1] or escolha == 1:
+        clear()
+        #Demonstração
+        Uteis.pg()
+        Uteis.pg(5, -5, 2)
+
+        #Inserindo Valores
+        i = int(input('Digite o INICIO de sua PG: '))
+        f = int(input('Digite o FIM da sua PG: '))
+        r = int(input('Digite a RAZÃO da sua PG: '))
+        Uteis.pg(i, f, r)
+        clear()
+
+    #Divisão entre Valor Inteiro e Real
+    elif escolha == EX[2] or escolha == 2:
+        #Inserindo valores
+        i = Uteis.leia_int()
+        r = Uteis.leia_float()
+
+        try: 
+            d = i/r
+            clear()
+            print(f'O valor INTEIRO é {i} e o valor REAL é {r} e a divisão entre eles é {d}')
+            Uteis.sleep(3)
+            clear()
+        except ZeroDivisionError: 
+            print('Não é possivel fazer divisão por ZERO.')
+
+        #Demonstrando Valores
+
+    #Verificando se URL está ativo
+    elif escolha == EX[3] or escolha == 3:
+        Uteis.check_url('https://www.pudim.com.br/')
+        while True:
+            try:
+                url = input('Digite o URL que deseja verificar: ')
+                Uteis.check_url(url)
+
+            #Tratamento
+            except ValueError:
+                if url == '':
+                    clear()
+                    break
+                print(f'Não é um URL valido, digite um URL existente.\n')
+            except Exception as erro:
+                print(f'ERRO>>> {erro.__cause__}')
+            
+    #Cadastro de pessoas
+    elif escolha == EX[4] or escolha == 4:
+        arquivo = 'cadastro.txt'
+        EX = ('Sair' ,'Ver pessoas cadastradas', 'Cadastrar nova pessoa')
+
+        clear()
+        titulo('Menu de Cadastros')
+        Uteis.arquivo_existe(arquivo)
+        while True:
+            menu = op_menu(EX, 'Cadastro')
+
+            #Sair
+            if menu == 0 or menu == 'Sair':
+                break
+            
+            #Ler Arquivo
+            if menu == 1 or menu == 'Ver pessoas cadastradas':
+                Uteis.ler_arquivo(arquivo)
+            
+            #Escrever no arquivo
+            if menu == 2 or menu == 'Cadastrar nova pessoa':
+                clear()
+                titulo('Novo Cadastro')
+                nome = Uteis.leia_str('Escreva o NOME da pessoa cadastrada: ', False)
+                idade = Uteis.leia_int('Escreva a IDADE da pessoa cadastrada: ')
+                Uteis.cadastrar_pessoa(arquivo, nome, idade)
+            
+#Curso {Python 3 Completo}
+    #Gerar CPF
+    elif escolha == EX[5] or escolha == 5:
+        cpf = input('Digite um CPF: '.center(90))
+        real_cpf = Uteis.verif_cpf(cpf)
+        print(real_cpf)
+        if real_cpf == True:
+            print(f'O CPF: {cpf} é verdadeiro')
+        else:
+            print(f'O CPF: {cpf} é falso')
+
+    
+
+
+
+    else:
+        print('Esta aula não existe'.center(90))
+
---
+++ Exercicios/Uteis.py
@@ -0,0 +1,166 @@
+import urllib
+import urllib.request
+import ssl
+from time import sleep
+from Menu import clear, titulo
+
+def leia_int(valor='Escreva um numero INTEIRO: ' ):
+    while True:
+        try:
+            i = int(input(valor))
+            return i
+        
+        #Tratamento
+        except ValueError:
+            print(f'\nPor favor, digite um numero Inteiro valido.')
+
+def leia_float(valor='Escreva um numero REAL: ' ):
+    while True:
+        try:
+            f = float(input(valor))
+            return f
+        
+        #Tratamento
+        except ValueError:
+            print(f'\nPor favor, digite um numero Real valido.')
+
+def leia_str(valor='Escreva um TEXTO: ', str_num=True):
+    while True:
+        try:
+            s = str(input(valor))
+            #Tratamento
+            if str_num == False:
+                try:
+                    int(s)
+                    print(f'{s}: Por favor, Digite um Texto valido.')
+                except:
+                    return s
+            else:
+                return s
+        except ValueError:
+            print(f'\n{s}: Por favor, Digite um Texto valido.')
+
+
+def pg(inicio=0, fim=10, razao=1, valor = 0):
+    valor += inicio
+
+    #Tratando Razão
+    if razao < 0:
+        razao *= -1
+    elif razao == 0:
+        razao = 1
+
+    print('-=-'*30)
+    print(f'Contagem de {inicio} até {fim} de {razao} em {razao}')
+    sleep(1)
+
+    #Razão Positiva
+    if inicio < fim:
+        while valor < fim+1:
+            print(valor, end='  ', flush=True)
+            sleep(0.3)
+            valor  += razao
+    #Razão Negativa
+    else:
+        while valor > fim-1:
+            print(valor, end='  ', flush=True)
+            sleep(0.3)
+            valor  -= razao
+
+    print('FIM!')
+    print('-=-'*30)
+    sleep(2)
+
+def check_url(url):
+    #Configurando URLLib para não checar Verificado
+    ctf = ssl.create_default_context()
+    ctf.check_hostname = False
+    ctf.verify_mode = ssl.CERT_NONE
+
+    try:
+        site = urllib.request.urlopen(url, context=ctf)
+
+    #Tratamento
+    except urllib.request.URLError:
+        print(f'\nO URL:{url}, está indisponivel no momento.\n')
+    except Exception as erro:
+        print(f'Algo deu errado.\nERRO>>>{erro.__class__}')
+    else:
+        print(f'\nO URL:{url}, está disponivel no momento.\n')
+    sleep(2)
+    clear()
+
+def arquivo_existe(nome):
+    try:
+        # Arquivo Existe?
+        arq = open(nome, 'rt')
+        arq.close()
+        print('Arquivo Existente.'.center(90))
+        return True
+    except:
+        # Tentando criar arquivo
+        try:
+            arq = open(nome, 'wt+')
+            arq.close()
+            print(f'Arquivo {nome} criado com sucesso.'.center(90))
+            return False
+        
+        #Tratamento
+        except Exception as erro: 
+            print(f'Houve um ERRO na criação do arquivo!\nERRO >>> {erro.__class__}')
+    
+def ler_arquivo(nome):
+    try:
+        arq = open(nome, 'rt')
+    except Exception as erro:
+        print(f'não foi possivel abrir o arquivo {nome}\nERRO>>>{erro.__class__}')
+    else:
+        clear()
+        titulo('Pessoas Cadastradas')
+        for linha in arq:
+            pessoa = linha.split(';')
+            print(f'Nome: {pessoa[0]:<70} Idade: {pessoa[1]:>2}')
+        sleep(5)
+        clear()
+    arq.close()
+
+def cadastrar_pessoa(arq, nome='desconhecido', idade='-x-'):
+    try:
+        arquivo = open(arq, 'at')
+    except:
+        print(f'Falha ao abrir o arquivo')
+    else:
+        try:
+            arquivo.write(f'\n{nome};{idade}')
+        except Exception as erro:
+            print(f'Algo deu errado na hora de escrever os dados.\nERRO>>> {erro.__class__}')
+    print(arq, nome, idade)
+
+def verif_cpf(cpf_bruto):
+    cpf_ref = cpf_bruto.replace(".", "").replace("-", "")
+    calculo = calculo_cpf(cpf_ref)
+    if cpf_ref[9] == str(calculo):
+        p1 = True
+    
+    calculo = calculo_cpf(cpf_ref, cont_regress=11)
+    if cpf_ref[10] == str(calculo):
+        p2 = True
+
+    if p1 and p2 == True:
+        return True
+
+def calculo_cpf(cpf ,limite=1, cont_regress=10, calculo=0):
+    for c in cpf:
+        mult = int(c) * cont_regress
+        calculo += mult
+        cont_regress -= 1
+        print(c, calculo)
+        if cont_regress == limite:
+            break
+    calculo = (calculo*10) % 11
+    if calculo >= 10: calculo = 0
+    print(calculo)
+    return calculo
+
+
+        
